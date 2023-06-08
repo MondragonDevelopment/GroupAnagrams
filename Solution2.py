7e164ce378e12f91c511605797250f3d38f98f6a
CultistaCoC <fimv705@gmail.com>
2023-06-08 02:43:53 -0600

    This is the solution I thought first, but I hadn't mastered dictionaries utilities yet

--- Solution2.py
+++ Solution2.py
@@ -5,11 +5,11 @@ strings = ["eat","tea","tan","ate","nat","bat"]
 def groupAnagrams(strs):
     dictionary = {}
     for word in strs:  # This array contains 26 zeroes (one for each letter)
-        sWord = "".join(sorted(word))
+        sWord = "".join(sorted(word)) # This is very weird, but you have to add "".join() in order to make sWord a string lmao
         if sWord in dictionary:
             dictionary[sWord].append(word)
         else:
-            dictionary[sWord] = [word]
+            dictionary[sWord] = [word] # This right-side brackets are needed for the dictionary to know that we are dealing with a list, not a string
     return list(dictionary.values())
 
 print(groupAnagrams(strings))
